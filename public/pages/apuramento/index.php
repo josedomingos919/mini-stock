8634e7d7f6066177973ec8e136dbfbdd779419b1
josedomingos919 <josedomingos919@gmail.com>
2021-08-28 17:16:58 +0100

    Update title

--- public/pages/apuramento/index.php
+++ public/pages/apuramento/index.php
@@ -12,7 +12,7 @@ require('./../../config/util.php');
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MiniStock</title>
+    <title>MiniStock | <?php echo $text ?></title>
     <link href="./style.css" rel="stylesheet" />
     <link rel="shortcut icon" href="./../../assets/img/icon.png" />
   </head>
