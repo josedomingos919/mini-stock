30e7330951d6e698a781fe8b5165c23edcf41799
josedomingos919 <josedomingos919@gmail.com>
2021-08-29 16:32:06 +0100

    update produto

--- public/pages/geral/produto/listar/index.php
+++ public/pages/geral/produto/listar/index.php
@@ -15,6 +15,9 @@ require('./../../../../config/util.php');
     <title>MiniStock | <?php echo $text ?></title>
     <link href="./style.css" rel="stylesheet" />
     <link rel="shortcut icon" href="./../../../../assets/img/icon.png" />
+    <script src="../../../../config/js/api.js"></script>
+    <script src="../../../../config/js/validate.js"></script>
+    <script src="./script.js"></script>
   </head>
   <body>
     <div class="app-container">
@@ -51,13 +54,13 @@ require('./../../../../config/util.php');
               <a style="color: #00000063;"  class="nav-link"  aria-current="page" href="/pages/geral/produto/"><i class="fas fa-box"></i> Produto</a>
             </li>
             <li class="nav-item">
-              <a  style="color: #fb6d3a;"  class="nav-link active" style="color: black;" aria-current="page" href="/pages/geral/produto/listar"><i class="fas fa-clipboard-list"></i> Listar Produtos</a>
+              <a   style="color: #fb6d3a;"   class="nav-link active" style="color: black;" aria-current="page" href="/pages/geral/produto/listar"><i class="fas fa-clipboard-list"></i> Listar Produtos <span id="totalData" ></span></a>
             </li> 
             <li class="nav-item">
-              <a  style="color: #00000063;"  class="nav-link" style="color: black;" aria-current="page" href="/pages/geral/categoria/"><i class="fas fa-filter"></i> Categoria</a>
+              <a   style="color: #00000063;" class="nav-link" style="color: black;" aria-current="page" href="/pages/geral/categoria/"><i class="fas fa-filter"></i> Categoria</a>
             </li> 
             <li class="nav-item">
-              <a  style="color: #00000063;"  class="nav-link" style="color: black;" aria-current="page" href="/pages/geral/categoria/listar/"><i class="fas fa-clipboard-list"></i> Listar Categoria</a>
+              <a  style="color: #00000063;"   class="nav-link " style="color: black;" aria-current="page" href="/pages/geral/categoria/listar/"><i class="fas fa-clipboard-list "></i> Listar Categoria</a>
             </li> 
           </ul>
         </section>
@@ -66,45 +69,32 @@ require('./../../../../config/util.php');
         <form class="mt-4 from-list">
           <table class="table table-striped table-hover">
               <thead>
-                <th>ID</th>
-                <th class="not-brack-text">Nome</th>
-                <th>Categoria</th>
-                <th>Quantidade</th>
-                <th>Preço Compra</th>
-                <th>Preço Venda</th>
-                <th>Preço Revendedor</th>
-                <th>Data</th>
-                <th>Editar</th>
-                <th>Eliminar</th>
-              </thead>
-              <tbody>
                 <tr>
-                  <td>lkjhg</td>
-                  <td class="not-brack-text">lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>lkjhg</td>
-                  <td>
-                  <button type="button" class="btn btn-light t"><i class="fas fa-pen-alt"></i></button>  
-             
-                  </td>
-                  <td>
-                    <button type="button" class="btn btn-primary"><i class="fas fa-trash-alt"></i></button>
-                  </td>
-                  
+                  <th>ID</th>
+                  <th class="not-brack-text">Foto</th>
+                  <th class="not-brack-text">Nome</th>
+                  <th>Data</th>
+                  <th>Editar</th>
+                  <th>Eliminar</th>
                 </tr>
-                
-
+              </thead>
+              <tbody id="table"> 
               </tbody>
           </table>
         </form>
+
+        <div class="mt-4">
+            <nav aria-label="Page navigation example mt-5">
+              <ul id="ulPagination" class="pagination justify-content-center">
+                
+              </ul>
+            </nav>
+        </div>
         
       </section>
     </div>
 
-    <script src="../../assets/libs/bootstrap-5.1.0-dist/js/bootstrap.js"></script>
+    <script src="../../../../assets/libs/bootstrap-5.1.0-dist/js/bootstrap.js"></script>
+    <script src="../../../components/loader/index.js"></script>
   </body>
 </html>
---
+++ public/pages/geral/produto/listar/script.js
@@ -0,0 +1,151 @@
+var produtoData = [];
+
+async function incialize(page = 1, limit = 5) {
+  const table = document.getElementById("table");
+  table.innerHTML = "";
+
+  _loader.show();
+  const response = await Api.all("produto", { page, limit });
+  _loader.hide();
+
+  if (response.data.length) {
+    const vet = response.data;
+    produtoData = vet;
+
+    table.innerHTML = vet
+      .map(
+        ({ data_, id, nome, foto }, index) => `
+        <tr>
+            <td>${id}</td>
+            <td><img class="img-prod" src="${FileLink + foto}" /></td>
+            <td class="not-brack-text" >${nome}</td>
+            <td>${data_}</td>
+            <td>
+                <button onclick="update(${index})" type="button" class="btn btn-light t"><i class="fas fa-pen-alt"></i></button>  
+            </td>
+            <td>
+                <button onclick="deleteItem(${index})" type="button" class="btn btn-primary"><i class="fas fa-trash-alt"></i></button>
+            </td> 
+        </tr>
+      `
+      )
+      .join(" ");
+
+    MakePagination(response);
+  } else {
+    alert("Nenhum dado foi encontrado!");
+  }
+}
+
+window.onload = async () => {
+  const params = getUrlparams();
+  incialize(params?.page, params?.limit);
+};
+
+function getUrlparams() {
+  try {
+    const result = {};
+
+    location.search
+      .substring(1)
+      .split("&")
+      .map((e) => ({
+        [e.split("=")?.[0]]: e.split("=")?.[1],
+      }))
+      .forEach((e) => {
+        if (Object.keys(e)?.[0])
+          result[Object.keys(e)?.[0]] = Object.values(e)?.[0];
+      });
+
+    return result;
+  } catch {
+    return {};
+  }
+}
+
+function MakePagination(response) {
+  const { page_all, page_now, all } = response;
+  const pageContainer = document.getElementById("ulPagination");
+  const spanTotal = document.getElementById("totalData");
+
+  console.log("response", response);
+
+  spanTotal.innerHTML = `( ${all} )`;
+
+  let paginatioRenderHtml = ``;
+
+  //Pagination
+  if (page_all > 1) {
+    for (let i = 1; i <= page_all; i++) {
+      paginatioRenderHtml += `
+            <li class="page-item">
+                <a ${
+                  i == page_now ? ` style="color: #fb6d3a;"` : ``
+                } class="page-link" href="/pages/geral/produto/listar/?page=${i}">${i}</a>
+            </li> 
+        `;
+    }
+  } else {
+    return ``;
+  }
+
+  //Back
+  if (+page_now !== 1) {
+    paginatioRenderHtml = `
+    <li class="page-item ">
+        <a class="page-link" href="/pages/geral/produto/listar/?page=${
+          +page_now - 1
+        }" tabindex="-1" aria-disabled="true"> « </a>
+    </li>
+    ${paginatioRenderHtml}
+`;
+  } else {
+    paginatioRenderHtml = `
+    <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="false"> « </a>
+    </li>
+    ${paginatioRenderHtml}
+`;
+  }
+
+  //Next
+  if (+page_now !== page_all) {
+    paginatioRenderHtml += `  
+        <li class="page-item">
+            <a class="page-link" href="/pages/geral/produto/listar/?page=${
+              +page_now + 1
+            }"> » </a>
+        </li>
+    `;
+  } else {
+    paginatioRenderHtml += `  
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="false"> » </a>
+        </li>
+    `;
+  }
+
+  //Render
+  pageContainer.innerHTML = paginatioRenderHtml;
+}
+
+async function deleteItem(index) {
+  const { id, foto } = produtoData[index];
+
+  if (confirm("Está preste a eliminar um item ?")) {
+    _loader.show();
+    const response = await Api.delete("produto", { id, unlink: foto });
+    if (response.status) {
+      alert("Eliminado com sucesso!");
+      location.reload();
+    } else {
+      alert("Falha ao eliminar!");
+    }
+    _loader.hide();
+  }
+}
+
+async function update(i) {
+  sessionStorage.setItem("produtoData", JSON.stringify(produtoData[i]));
+  location.href = "/pages/geral/produto/";
+}
