023b4c882c50731b0f7527fd2642c733312116d1
josedomingos919 <josedomingos919@gmail.com>
2021-08-29 20:19:56 +0100

    Update produto

---
+++ public/pages/venda/vender/index.php
@@ -0,0 +1,95 @@
+<?php 
+
+session_start(); 
+$_SESSION['active_routes'] = 1;
+require('./../../../config/util.php');
+
+?> 
+
+<!DOCTYPE html>
+<html lang="pt-pt">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MiniStock | <?php echo $text ?></title>
+    <link href="./style.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="./../../../assets/img/icon.png" />
+    <script src="../../../config/js/api.js"></script>
+    <script src="../../../config/js/validate.js"></script>
+    <script src="./script.js"></script>
+  </head>
+  <body>
+    <div class="app-container">
+      <section class="menu">
+        <div class="container-user">
+          <div class="user-log">M</div>
+          <div class="log-text">iniStock</div>
+        </div>
+
+        <?php 
+            render_routes();
+        ?> 
+
+        <div class="container-footer">
+          <div class="div-text">Ajuda 24h/24h</div>
+          <div class="div-sub-text">Contacte nos todo tempo!</div>
+          <div class="div-buttom">Iniciar</div>
+        </div>
+      </section>
+
+      <section class="app">
+        <div class="container-header-title">
+          <h4 class="title-page mr-3">
+            <i class="fab fa-buffer"></i>
+            <label class="teste-ye"><?php echo $text ?></label>
+          </h4>
+          <hr />
+        </div>
+
+        <!--Header-->
+        <section class="mt-4">
+          <ul class="nav nav-tabs"> 
+            <li class="nav-item">
+              <a   style="color: #fb6d3a;"   class="nav-link active" style="color: black;" aria-current="page" href="/pages/geral/produto/listar"><i class="fas fa-clipboard-list"></i> Produtos</a>
+            </li> 
+            <li class="nav-item">
+              <a   style="color: #00000063;" class="nav-link" style="color: black;" aria-current="page" href="/pages/geral/categoria/"><i class="fas fa-filter"></i> Categoria</a>
+            </li>  
+          </ul>
+        </section>
+
+        <!--Body-->
+        <form class="mt-4 from-list">
+          <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th class="not-brack-text">Foto</th>
+                  <th class="not-brack-text">Nome</th>
+                  <th>Preço</th> 
+                  <th>Quantidade</th> 
+                  <th>Total</th>  
+                  <th  style="text-align: center;" >Adicionar</th>  
+                </tr>
+              </thead>
+              <tbody id="table"> 
+              </tbody>
+          </table>
+        </form>
+
+        <div class="mt-4">
+            <nav aria-label="Page navigation example mt-5">
+              <ul id="ulPagination" class="pagination justify-content-center">
+                
+              </ul>
+            </nav>
+        </div>
+        
+      </section>
+    </div>
+
+    <script src="../../../assets/libs/bootstrap-5.1.0-dist/js/bootstrap.js"></script>
+    <script src="../../components/loader/index.js"></script>
+  </body>
+</html>
---
+++ public/pages/venda/vender/script.js
@@ -0,0 +1,139 @@
+var produtoData = [];
+
+async function incialize(page = 1, limit = 5) {
+  const table = document.getElementById("table");
+  table.innerHTML = "";
+
+  _loader.show();
+  const response = await Api.all("produto", {
+    page,
+    limit,
+    moreTable: "categoria.id produto.id_categoria",
+    leftJoin: true,
+  });
+  _loader.hide();
+
+  if (response.data.length) {
+    const vet = response.data;
+    produtoData = vet;
+
+    table.innerHTML = vet
+      .map(
+        ({ data_, id, nome, foto, preco_venda }, index) => `
+        <tr>
+            <td>${id}</td>
+            <td><img class="img-prod" src="${FileLink + foto}" /></td>
+            <td class="not-brack-text" >${nome}</td> 
+            <td>${formatNumber(preco_venda)} AOA</td> 
+            <td><input onchange="calcularTotal(${index},this.value)"  onkeyup="calcularTotal(${index},this.value)"  style="width: 88px;" class="form-control form-control-sm" type="number" min="1" aria-label=".form-control-sm example"> </td>
+            <td id="tdTotal${index}" > - </td>
+            <td style="text-align: center;">
+                <button onclick="update(${index})" type="button" class="btn btn-light t"><i class="fas fa-plus"></i></button>  
+            </td> 
+        </tr>
+      `
+      )
+      .join(" ");
+
+    MakePagination(response);
+  } else {
+    alert("Nenhum dado foi encontrado!");
+  }
+}
+
+window.onload = async () => {
+  const params = getUrlparams();
+  incialize(params?.page, params?.limit);
+};
+
+function getUrlparams() {
+  try {
+    const result = {};
+
+    location.search
+      .substring(1)
+      .split("&")
+      .map((e) => ({
+        [e.split("=")?.[0]]: e.split("=")?.[1],
+      }))
+      .forEach((e) => {
+        if (Object.keys(e)?.[0])
+          result[Object.keys(e)?.[0]] = Object.values(e)?.[0];
+      });
+
+    return result;
+  } catch {
+    return {};
+  }
+}
+
+function MakePagination(response) {
+  const { page_all, page_now } = response;
+  const pageContainer = document.getElementById("ulPagination");
+
+  console.log("response", response);
+
+  let paginatioRenderHtml = ``;
+
+  //Pagination
+  if (page_all > 1) {
+    for (let i = 1; i <= page_all; i++) {
+      paginatioRenderHtml += `
+            <li class="page-item">
+                <a ${
+                  i == page_now ? ` style="color: #fb6d3a;"` : ``
+                } class="page-link" href="/pages/geral/produto/listar/?page=${i}">${i}</a>
+            </li> 
+        `;
+    }
+  } else {
+    return ``;
+  }
+
+  //Back
+  if (+page_now !== 1) {
+    paginatioRenderHtml = `
+    <li class="page-item ">
+        <a class="page-link" href="/pages/geral/produto/listar/?page=${
+          +page_now - 1
+        }" tabindex="-1" aria-disabled="true"> « </a>
+    </li>
+    ${paginatioRenderHtml}
+`;
+  } else {
+    paginatioRenderHtml = `
+    <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="false"> « </a>
+    </li>
+    ${paginatioRenderHtml}
+`;
+  }
+
+  //Next
+  if (+page_now !== page_all) {
+    paginatioRenderHtml += `  
+        <li class="page-item">
+            <a class="page-link" href="/pages/geral/produto/listar/?page=${
+              +page_now + 1
+            }"> » </a>
+        </li>
+    `;
+  } else {
+    paginatioRenderHtml += `  
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="false"> » </a>
+        </li>
+    `;
+  }
+
+  //Render
+  pageContainer.innerHTML = paginatioRenderHtml;
+}
+
+async function calcularTotal(index, value) {
+  const { preco_venda } = produtoData[index];
+  const td = document.getElementById("tdTotal" + index);
+
+  const total = formatNumber(preco_venda * value);
+  td.innerHTML = total ? total + " AOA" : "-";
+}
